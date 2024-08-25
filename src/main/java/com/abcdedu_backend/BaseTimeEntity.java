f6db50797ef2df1218a6032a6643f526ebc22443
sujeong <sj980204@gmail.com>
2024-08-25 22:01:06 +0900

    [feat] #4 BaseTimeEntity - 생성 시간, 마지막 수정 시간 필드 자동생성

---
+++ src/main/java/com/abcdedu_backend/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.abcdedu_backend;
+
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
