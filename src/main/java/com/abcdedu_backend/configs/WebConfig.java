703f33a4151bb7bc3b49a1a4a69457c6b01ca840
cheon4050 <ehdcjs159@gmail.com>
2024-08-30 00:16:43 +0900

    [fix] #17 리엑트 개발환경에서만 cors 허용하게 수정

--- src/main/java/com/abcdedu_backend/configs/WebConfig.java
+++ src/main/java/com/abcdedu_backend/configs/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedHeaders("*")
                 .allowedMethods(
                     HttpMethod.GET.name(),
