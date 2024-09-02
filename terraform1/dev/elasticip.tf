f7a92000c852d09869466885db029aa04d8f265b
cheon4050 <ehdcjs159@gmail.com>
2024-09-03 01:55:50 +0900

    [fix] #35 기존 테라폼 방식 삭제

--- terraform1/dev/elasticip.tf
+++
@@ -1,4 +0,0 @@
-# Elastic IP 생성
-resource "aws_eip" "web_eip" {
-  instance = aws_instance.ec2.id
-}
--- terraform1/dev/pemkey.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "aws_pem_key" {
-  key_name   = "abcdedu-dev-pem-key"
-  public_key = var.public_key
-}
