cf91364b7b522f1ce5db34e2148967746c7a2efb
cheon4050 <ehdcjs159@gmail.com>
2024-09-02 16:07:31 +0900

    [refactor] #20 사용하지 않는 파라미터 제거

--- src/main/java/com/abcdedu_backend/lecture/dto/request/CreateAssignmentAnswerRequest.java
+++ src/main/java/com/abcdedu_backend/lecture/dto/request/CreateAssignmentAnswerRequest.java
@@ -5,9 +5,6 @@ import com.abcdedu_backend.lecture.dto.CreateAssignmentAnswerDto;
 import java.util.List;
 
 public record CreateAssignmentAnswerRequest (
-        String school,
-        Long studentId,
-        String name,
         List<CreateAssignmentAnswerDto> answers
 ){
 }
