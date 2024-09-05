b931d227bf5af719a28f4c6eee356400a709e950
SUJEONG CHOI <sj980204@gmail.com>
2024-09-05 20:48:00 +0900

    [fix] #40 ErrorCode 역할별 에러 통일화 (#41)

--- src/main/java/com/abcdedu_backend/exception/ErrorCode.java
+++ src/main/java/com/abcdedu_backend/exception/ErrorCode.java
@@ -3,34 +3,28 @@ package com.abcdedu_backend.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
     // 역할별 에러
-    ROLE_INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "등업이 필요합니다."),
+    STUDENT_VALID_PERMISSION(HttpStatus.FORBIDDEN, "학생등급 이상 가능"),
+    ADMIN_VALID_PERMISSION(HttpStatus.FORBIDDEN, "관리자 전용"),
+    ADMIN_OR_WRITER_PERMISSION(HttpStatus.FORBIDDEN, "본인과 관리자 전용"),
 
     // 상담
     CONTACT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상담이 없습니다."),
-    CONTACT_INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "본인과 관리자만 가능한 기능입니다."),
-
 
     // 게시판
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 포스트가 없습니다."),
-    POST_INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "본인과 관리자만 가능한 기능입니다."),
     POST_NOT_ALLOWED_COMMENT(HttpStatus.FORBIDDEN, "댓글을 허용하지 않는 게시글 입니다."),
 
     // 댓글
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 댓글이 없습니다."),
-    COMMENT_INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "본인과 관리자만 가능한 기능입니다."),
 
 
     // 카테고리
     BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 카테고리를 찾을 수 없습니다."),
     BOARD_DUPLICATION(HttpStatus.CONFLICT, "이미 존재하는 카테고리입니다."),
-    BOARD_INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "본인과 관리자만 가능한 기능입니다."),
-
 
     //회원
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
@@ -39,9 +33,6 @@ public enum ErrorCode {
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다."),
     TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다."),
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
-
-    //멤버
-    UNAUTHORIZED_ACCESS(HttpStatus.FORBIDDEN, "해당 권한이 없습니다."),
 
     //클래스
     CLASS_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 클래스입니다."),
