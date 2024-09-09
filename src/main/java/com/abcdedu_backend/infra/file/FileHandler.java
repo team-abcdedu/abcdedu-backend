c2dc3aaa6ead0bb80df01347b2508abf3f583e7e
cheon4050 <ehdcjs159@gmail.com>
2024-09-09 21:40:13 +0900

    [fix] #39 같은 이름 덮어쓰기 방지

--- src/main/java/com/abcdedu_backend/infra/file/FileHandler.java
+++ src/main/java/com/abcdedu_backend/infra/file/FileHandler.java
@@ -2,8 +2,6 @@ package com.abcdedu_backend.infra.file;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 public interface FileHandler {
 
     String upload(MultipartFile file, FileDirectory directory, String fileName);
