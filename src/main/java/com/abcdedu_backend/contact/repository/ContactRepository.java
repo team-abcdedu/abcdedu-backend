309405457b6043c55b55e5c755459778a282ff8b
sujeong <sj980204@gmail.com>
2024-09-12 12:19:24 +0900

    [feat] #60 contact 목록 조회에 페이징 기능 추가

--- src/main/java/com/abcdedu_backend/contact/repository/ContactRepository.java
+++ src/main/java/com/abcdedu_backend/contact/repository/ContactRepository.java
@@ -1,7 +1,11 @@
 package com.abcdedu_backend.contact.repository;
 
 import com.abcdedu_backend.contact.entity.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    Page<Contact> findAll(Pageable pageable);
 }
