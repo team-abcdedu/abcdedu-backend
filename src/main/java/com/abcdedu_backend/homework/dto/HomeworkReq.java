cbf50d1661e283e6854aacca70b8428b7b81bcc5
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-13 14:06:54 +0900

    [fix]: 과제 API 스펙 수정

--- src/main/java/com/abcdedu_backend/homework/dto/HomeworkReq.java
+++ src/main/java/com/abcdedu_backend/homework/dto/HomeworkReq.java
@@ -11,18 +11,15 @@ public class HomeworkReq {
         @Schema(example ="사회 문제 해결을 위한 인공지능 설계")
         @NotBlank
         String title,
-        @Schema(example = """
-            <h2>[인공지능으로 사회문제를 해결한 사례 시청]</h2>
-            <p>다양한 사례를 통해 인공지능이 사회문제를 해결하기 위해 어떻게 사용되는지 알아보자.
-            그리고 다음 과제를 수행해보자.</p>""")
+        @Schema(example = "[인공지능으로 사회문제를 해결한 사례 시청]")
+        @NotBlank
+        String subTitle,
+        @Schema(example = "다양한 사례를 통해 인공지능이 사회문제를 해결하기 위해 어떻게 사용되는지 알아보자. " +
+            "그리고 다음 과제를 수행해보자.")
         @NotBlank
         String description,
         @NotNull
         List<HomeworkQuestionReq.Create> questions,
-        @Schema(example = """
-            <h3>[토론 및 발표]</h3>
-            <p>인공지능을 활용하여 사회문제를 해결하기 위한 방법을 발표하고 토론해보자.</p>
-            """)
         @NotNull
         String additionalDescription
     ) {}
@@ -30,6 +27,8 @@ public class HomeworkReq {
     public record Update(
         @NotBlank
         String title,
+        @NotBlank
+        String subTitle,
         @NotBlank
         String description,
         @NotNull
