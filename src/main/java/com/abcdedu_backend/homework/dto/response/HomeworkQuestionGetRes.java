3dc9128fabf38074ef4aa25b866a86164f202d96
SUJEONG CHOI <sj980204@gmail.com>
2024-09-20 03:57:45 +0900

    [FIX] #93 게시글, 댓글, 과제 관련 오류 해결 (#95)
    * [fix] #93 homework 질문을 description, additionaldescription 으로 분리
    
    * [fix] #93 게시글 생성, 수정 시 파일 빈 문자열일 때 500 오류
    - file이 안 들어오면 문자열로 전환하여 post에 저장한다.
    - 조회 시에도 변환 로직을 넣어준다.
    
    * [fix] #93 게시글 상세 조회, 댓글 목록 content에 email 포함
    
    * [fix] #93 댓글 목록 응답에 commentId 추가

--- src/main/java/com/abcdedu_backend/homework/dto/response/HomeworkQuestionGetRes.java
+++ src/main/java/com/abcdedu_backend/homework/dto/response/HomeworkQuestionGetRes.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 public record HomeworkQuestionGetRes(
         String orderNumber, // 보여질 문제 순서
         String content, // 문제 내용
+        String additionalContent, // 문제 내용 2
         boolean isAnswerRequired // 필수 답변 여부
 
 ) {
--- src/main/java/com/abcdedu_backend/post/controller/PostController.java
+++ src/main/java/com/abcdedu_backend/post/controller/PostController.java
@@ -56,7 +56,7 @@ public class PostController {
         return Response.success(postService.getPost(postId, memberId));
     }
 
-    @PostMapping("/")
+    @PostMapping()
     @Operation(summary = "게시글 생성", description = "게시글을 작성합니다. 역할이 학생이상이여야만 작성이 가능합니다.")
 //    @ApiResponses(value = {
 //            @ApiResponse(responseCode = "400", description = "공백 요청 불가 : 제목", content = @Content),
