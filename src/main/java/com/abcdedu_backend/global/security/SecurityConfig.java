78b14e8b1c710f20bfe621f3f7901a833de4ca84
cheon4050 <ehdcjs159@gmail.com>
2024-10-15 18:47:37 +0900

    [fix] #125 security 적용 안된 500에러 수정

--- src/main/java/com/abcdedu_backend/global/security/SecurityConfig.java
+++ src/main/java/com/abcdedu_backend/global/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
@@ -26,12 +25,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -86,7 +81,7 @@ public class SecurityConfig {
                 "/actuator/**"
             ).permitAll()
             .requestMatchers("/admin/**").hasRole(MemberRole.ADMIN.name()) // ADMIN 권한만 접근 가능
-            .anyRequest().permitAll() // TODO
+            .anyRequest().authenticated() // TODO
         );
 
         /**
