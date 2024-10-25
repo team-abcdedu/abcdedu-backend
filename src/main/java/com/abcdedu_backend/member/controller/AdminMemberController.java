e7146cec680f60be27ebe47866b274678cf6a370
sujeong <sj980204@gmail.com>
2024-10-26 00:36:30 +0900

    [feat] #141 멤버 일괄 등급 변경 API 설계

--- src/main/java/com/abcdedu_backend/member/controller/AdminMemberController.java
+++ src/main/java/com/abcdedu_backend/member/controller/AdminMemberController.java
@@ -3,16 +3,16 @@ package com.abcdedu_backend.member.controller;
 import com.abcdedu_backend.common.jwt.JwtValidation;
 import com.abcdedu_backend.common.page.request.PagingRequest;
 import com.abcdedu_backend.common.page.request.SortRequest;
+import com.abcdedu_backend.member.dto.request.ChangeMemberRoleRequest;
 import com.abcdedu_backend.member.dto.request.MemberSearchCondition;
 import com.abcdedu_backend.member.dto.response.AdminSearchMemberResponse;
+import com.abcdedu_backend.member.entity.MemberRole;
 import com.abcdedu_backend.utils.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +27,11 @@ public class AdminMemberController {
     @GetMapping("/search")
     public Response<List<AdminSearchMemberResponse>> getmembersByCondition(@JwtValidation Long memberId, PagingRequest pagingRequest, SortRequest sortRequest, MemberSearchCondition cond){
         return Response.success(null);
+    }
+    @Operation(summary = "멤버 일괄 등업")
+    @PatchMapping("/role/{roleName}")
+    public Response<Void> changeMembersRole(@JwtValidation Long memberId, @PathVariable MemberRole roleName,  @RequestBody List<ChangeMemberRoleRequest> requests) {
+        return Response.success();
     }
 
 }
