3cb84bf0fd869013f65c0805acc597ffcd1f7e19
sujeong <sj980204@gmail.com>
2024-10-26 01:05:28 +0900

    [feat] #141 PagedResponse 추가 - 일관된 page 응답을 위해

--- src/main/java/com/abcdedu_backend/member/controller/AdminMemberController.java
+++ src/main/java/com/abcdedu_backend/member/controller/AdminMemberController.java
@@ -1,12 +1,15 @@
 package com.abcdedu_backend.member.controller;
 
 import com.abcdedu_backend.common.jwt.JwtValidation;
+import com.abcdedu_backend.common.page.PageManager;
 import com.abcdedu_backend.common.page.request.PagingRequest;
 import com.abcdedu_backend.common.page.request.SortRequest;
+import com.abcdedu_backend.common.page.response.PagedResponse;
 import com.abcdedu_backend.member.dto.request.ChangeMemberRoleRequest;
 import com.abcdedu_backend.member.dto.request.MemberSearchCondition;
 import com.abcdedu_backend.member.dto.response.AdminSearchMemberResponse;
 import com.abcdedu_backend.member.entity.MemberRole;
+import com.abcdedu_backend.member.service.AdminMemberService;
 import com.abcdedu_backend.utils.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,10 +26,12 @@ import java.util.List;
 @Tag(name = "관리자 멤버 관리 기능")
 
 public class AdminMemberController {
+
+    private final AdminMemberService adminMemberService;
     @Operation(summary = "멤버 조회", description = "필터링, ")
     @GetMapping("/search")
-    public Response<List<AdminSearchMemberResponse>> getmembersByCondition(@JwtValidation Long memberId, @RequestBody PagingRequest pagingRequest,
-                                                                           @RequestBody SortRequest sortRequest, @RequestBody MemberSearchCondition cond){
+    public Response<PagedResponse<AdminSearchMemberResponse>> getmembersByCondition(@JwtValidation Long memberId, @RequestBody PagingRequest pagingRequest, @RequestBody SortRequest sortRequest, @RequestBody MemberSearchCondition cond){
+        adminMemberService.searchMembers(memberId, new PageManager(pagingRequest, sortRequest).makePageRequest(), cond);
         return Response.success(null);
     }
     @Operation(summary = "멤버 일괄 등업")
