3e3457433154fcca04a0276c1f79da3b2a357f3e
SUJEONG CHOI <sj980204@gmail.com>
2024-10-26 10:13:56 +0900

    [FIX] 클래스 commit 누락으로 빌드 실패된 것 수정  (#148)
    * [fix] #141 빌드 실패 service 클래스 추가

---
+++ src/main/java/com/abcdedu_backend/member/service/AdminMemberService.java
@@ -0,0 +1,12 @@
+package com.abcdedu_backend.member.service;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class AdminMemberService {
+
+
+}
