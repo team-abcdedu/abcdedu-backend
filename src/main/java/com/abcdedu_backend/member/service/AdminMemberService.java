bdc87f232fb6713c8b603bbf850f92793cec60ff
sujeong <sj980204@gmail.com>
2024-10-26 10:15:26 +0900

    [fix] #141 빌드 실패 해결

--- src/main/java/com/abcdedu_backend/member/service/AdminMemberService.java
+++ src/main/java/com/abcdedu_backend/member/service/AdminMemberService.java
@@ -1,11 +1,9 @@
 package com.abcdedu_backend.member.service;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@AllArgsConstructor
 public class AdminMemberService {
 
 
