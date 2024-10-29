e5fd155c77b9b1f2929babc28d5e2646d5426254
cheon4050 <ehdcjs159@gmail.com>
2024-10-29 15:52:21 +0900

    [fix] #152 패스워드 수정시 트랜잭션 추가

--- src/main/java/com/abcdedu_backend/member/service/EmailService.java
+++ src/main/java/com/abcdedu_backend/member/service/EmailService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 
@@ -39,6 +40,7 @@ public class EmailService {
         }
     }
 
+    @Transactional
     public void sendTempPasswordToEmail(String toEmail) {
         String tempPassword = randomCodeGenerator.generatePassword();
         String text = String.format(SEND_TEMP_PASSWORD_TEXT_FORM, tempPassword);
