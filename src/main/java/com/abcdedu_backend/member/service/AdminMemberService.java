e920480e6956e9df582457dae57037016cab21a7
sujeong <sj980204@gmail.com>
2024-10-29 10:21:54 +0900

    [feat] #154 관리자 전용 전체 멤버 조회 기능 개발 - @query 사용하여 동적 쿼리 생성

--- src/main/java/com/abcdedu_backend/member/service/AdminMemberService.java
+++ src/main/java/com/abcdedu_backend/member/service/AdminMemberService.java
@@ -1,11 +1,46 @@
 package com.abcdedu_backend.member.service;
-import lombok.AllArgsConstructor;
+import com.abcdedu_backend.exception.ApplicationException;
+import com.abcdedu_backend.exception.ErrorCode;
+import com.abcdedu_backend.member.dto.request.MemberSearchCondition;
+import com.abcdedu_backend.member.dto.response.AdminSearchMemberResponse;
+import com.abcdedu_backend.member.entity.Member;
+import com.abcdedu_backend.member.repository.AdminMemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class AdminMemberService {
 
+    private final AdminMemberRepository memberRepository;
 
+    public Page<AdminSearchMemberResponse> searchMembers(Long memberId, Pageable pageable, MemberSearchCondition cond) {
+        Member member = checkMember(memberId);
+        checkAdminPermission(member);
+        log.info("권한 확인 성공 : {}", member.getRole());
+
+        Page<Member> members = memberRepository.findAllByCondition(
+                cond.school(),
+                cond.studentId(),
+                cond.name(),
+                cond.role(),
+                pageable);
+
+        log.info("필터에 맞는 member 조회 성공");
+        return members.map(AdminSearchMemberResponse::fromMember);
+    }
+
+    private Member checkMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new ApplicationException(ErrorCode.USER_NOT_FOUND));
+    }
+    private void checkAdminPermission(Member member) {
+        if (!member.isAdmin()){
+            throw new ApplicationException(ErrorCode.ADMIN_VALID_PERMISSION);
+        }
+    }
 }
