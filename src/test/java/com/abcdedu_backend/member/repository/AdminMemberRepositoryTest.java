81183dfdcc4aba9d47b5cf73e8a6560906d7b022
sujeong <sj980204@gmail.com>
2024-10-30 19:41:35 +0900

    [feat] #156 jpa data 관련 함수가 잘 작동하는지 testcode 추가

--- src/test/java/com/abcdedu_backend/member/repository/AdminMemberRepositoryTest.java
+++ src/test/java/com/abcdedu_backend/member/repository/AdminMemberRepositoryTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -20,9 +23,9 @@ class AdminMemberRepositoryTest {
     private MemberRepository memberRepository;
 
     void setUp() {
-        Member member1 = new Member(null, "basic1", "basic1@example.com", "password1004", "Basic School", 1001L, "key1", MemberRole.BASIC, null, null);
-        Member member2 = new Member(null, "basic2", "basic2@example.com", "password1004", "Basic School", 1002L, "key2", MemberRole.BASIC, null, null);
-        Member member3 = new Member(null, "admin", "admin@example.com", "password1004", "Admin School", 1003L, "key3", MemberRole.ADMIN, null, null);
+        Member member1 = new Member(1L, "basic1", "basic1@example.com", "password1004", "Basic School", 1001L, "key1", MemberRole.BASIC, null, null);
+        Member member2 = new Member(2L, "basic2", "basic2@example.com", "password1004", "Basic School", 1002L, "key2", MemberRole.BASIC, null, null);
+        Member member3 = new Member(3L, "admin", "admin@example.com", "password1004", "Admin School", 1003L, "key3", MemberRole.ADMIN, null, null);
         memberRepository.save(member1);
         memberRepository.save(member2);
         memberRepository.save(member3);
@@ -75,6 +78,24 @@ class AdminMemberRepositoryTest {
         assertThat(result.getTotalElements()).isEqualTo(1);
         assertThat(result.getContent().get(0).getName()).isEqualTo("basic1");
         assertThat(result.getContent().get(0).getSchool()).isEqualTo("Basic School");
+    }
+
+    @Test
+    void findAllByIdIn_조건에_맞는_멤버_조회_성공() {
+        setUp();
+
+        List<Member> result = adminMemberRepository.findAllByIdIn(List.of(1L, 2L));
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    void findAllByIdIn_조건에_맞는_멤버_없으면_빈값이_반환() {
+        setUp();
+
+        List<Member> result = adminMemberRepository.findAllByIdIn(List.of(4L, 5L));
+
+        assertThat(result.size()).isEqualTo(0);
     }
 
 
