d2f1fdf83565a12bd73a26201891d2910d54a385
sujeong <sj980204@gmail.com>
2024-10-30 18:40:46 +0900

    [refact] #154 /admin 경로 필터 적용으로 관리자 권한 확인 로직 제거

--- src/main/java/com/abcdedu_backend/member/service/AdminMemberService.java
+++ src/main/java/com/abcdedu_backend/member/service/AdminMemberService.java
@@ -1,6 +1,4 @@
 package com.abcdedu_backend.member.service;
-import com.abcdedu_backend.exception.ApplicationException;
-import com.abcdedu_backend.exception.ErrorCode;
 import com.abcdedu_backend.member.dto.request.MemberSearchCondition;
 import com.abcdedu_backend.member.dto.response.AdminSearchMemberResponse;
 import com.abcdedu_backend.member.entity.Member;
@@ -18,29 +16,14 @@ public class AdminMemberService {
 
     private final AdminMemberRepository memberRepository;
 
-    public Page<AdminSearchMemberResponse> searchMembers(Long memberId, Pageable pageable, MemberSearchCondition cond) {
-        Member member = checkMember(memberId);
-        checkAdminPermission(member);
-        log.info("권한 확인 성공 : {}", member.getRole());
-
+    public Page<AdminSearchMemberResponse> searchMembers(Pageable pageable, MemberSearchCondition cond) {
         Page<Member> members = memberRepository.findAllByCondition(
                 cond.school(),
                 cond.studentId(),
                 cond.name(),
                 cond.role(),
                 pageable);
-
         log.info("필터에 맞는 member 조회 성공");
         return members.map(AdminSearchMemberResponse::fromMember);
-    }
-
-    private Member checkMember(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> new ApplicationException(ErrorCode.USER_NOT_FOUND));
-    }
-    private void checkAdminPermission(Member member) {
-        if (!member.isAdmin()){
-            throw new ApplicationException(ErrorCode.ADMIN_VALID_PERMISSION);
-        }
     }
 }
