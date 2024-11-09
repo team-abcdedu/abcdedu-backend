a4c8e3bddf85ea0615c188e7a0d2a3d0c881ff17
sujeong <sj980204@gmail.com>
2024-11-09 22:36:48 +0900

    Revert "[FEAT] #136 과제 응답 조회 (#178)"
    This reverts commit 7547c0ddbe5b9f98eda6c311b4439dd8bb66fc9b.

--- src/main/java/com/abcdedu_backend/homework/repository/HomeworkReplyRepository.java
+++ src/main/java/com/abcdedu_backend/homework/repository/HomeworkReplyRepository.java
@@ -2,19 +2,10 @@ package com.abcdedu_backend.homework.repository;
 
 import com.abcdedu_backend.homework.entity.Homework;
 import com.abcdedu_backend.homework.entity.HomeworkReply;
-import com.abcdedu_backend.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface HomeworkReplyRepository extends JpaRepository<HomeworkReply, Long> {
-    List<HomeworkReply> findByHomeworkAndMember(Homework homework, Member member);
-
-    @Query("SELECT r FROM HomeworkReply r " +
-                  "JOIN FETCH r.member m " +
-                  "JOIN FETCH r.homeworkQuestion q " +
-                  "WHERE r.homework.id = :homeworkId")
-    List<HomeworkReply> findRepliesByHomeworkId(@Param("homeworkId") Long homeworkId);
+    List<HomeworkReply> findAllByHomework(Homework homework);
 }
