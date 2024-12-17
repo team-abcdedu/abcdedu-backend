f759b5452e0707eb26b2e655e39f466407862c1e
sujeong <sj980204@gmail.com>
2024-12-17 15:24:48 +0900

    [refactor] #206 애노테이션으로 변환 가능한 코드 애노테이션으로 변환

--- src/main/java/com/abcdedu_backend/utils/exportable/ExcelData.java
+++ src/main/java/com/abcdedu_backend/utils/exportable/ExcelData.java
@@ -1,29 +1,16 @@
 package com.abcdedu_backend.utils.exportable;
 
 
-import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+@Getter
+@RequiredArgsConstructor
 public class ExcelData implements ExportDataProvider {
     private final String workbookName;
     private final List<String> headerName;
 
     private final List<List<Object>> rowData;
 
-    public ExcelData(final String workbookName, final List<String> headerName, final List<List<Object>> rowData) {
-        this.workbookName = workbookName;
-        this.headerName = headerName;
-        this.rowData = rowData;
-    }
-
-    public String getWorkbookName() {
-        return workbookName;
-    }
-
-    public List<String> getHeaderName() {
-        return headerName;
-    }
-
-    public List<List<Object>> getRowData() {
-        return rowData;
-    }
 }
