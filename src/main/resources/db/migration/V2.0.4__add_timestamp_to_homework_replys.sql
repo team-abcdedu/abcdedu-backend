c833ede915f28d1fb0b13caf3ed25f2028c1ab87
sujeong <sj980204@gmail.com>
2024-12-18 03:28:42 +0900

    [fix] mysql에 맞지 않았던 쿼리문 수정

--- src/main/resources/db/migration/V2.0.4__add_timestamp_to_homework_replys.sql
+++ src/main/resources/db/migration/V2.0.4__add_timestamp_to_homework_replys.sql
@@ -5,10 +5,5 @@ ALTER TABLE homework_replys
 ALTER TABLE homework_replys
     ADD COLUMN updated_at TIMESTAMP;
 
--- 기존 데이터에 필드 기본값 업데이트 : 자동 집계되는 날짜와 비교 하기 위해 특정 이전값으로 설정
-UPDATE homework_replys
-SET created_at = '2024-11-01 10:00:00',
-    updated_at = '2024-11-01 10:00:00'
-WHERE created_at IS NULL OR updated_at IS NULL;
 
 
