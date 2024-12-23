1d859eeda68898856ad1868df429fca98f6d9e11
SUJEONG CHOI <sj980204@gmail.com>
2024-12-23 21:18:15 +0900

    [FIX] 1회차 homework 프론트 개발자 건의사항 반영 (#217)
    * [fix] #214 homework 응답 엑셀로 다운로드 기능 요청시 body 에서 쿼리 파라미터로 변경
    - Get요청이므로 파라미터로 전달하는 것이 올바름
    
    * [fix] #214 homework 목록 조회 기능 에 페이징 기능 추가

--- src/main/java/com/abcdedu_backend/homework/controller/AdminHomeworkController.java
+++ src/main/java/com/abcdedu_backend/homework/controller/AdminHomeworkController.java
@@ -1,4 +1,8 @@
 package com.abcdedu_backend.homework.controller;
+import com.abcdedu_backend.common.page.PageManager;
+import com.abcdedu_backend.common.page.request.PagingRequest;
+import com.abcdedu_backend.common.page.request.SortRequest;
+import com.abcdedu_backend.common.page.response.PagedResponse;
 import com.abcdedu_backend.homework.dto.response.HomeworkRes;
 import com.abcdedu_backend.homework.service.HomeworkService;
 import com.abcdedu_backend.utils.Response;
@@ -6,11 +10,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,7 +26,9 @@ public class AdminHomeworkController {
     private final HomeworkService homeworkService;
     @Operation(summary = "과제 목록", description = "과제 목록을 조회 합니다.")
     @GetMapping
-    public Response<List<HomeworkRes>> readList() {
-        return Response.success(homeworkService.getHomeworks());
+    public Response<PagedResponse<HomeworkRes>> read(PagingRequest pagingRequest, SortRequest sortRequest) {
+        PageRequest pageRequest = new PageManager(pagingRequest, sortRequest).makePageRequest();
+        Page<HomeworkRes> homeworks = homeworkService.getHomeworks(pageRequest);
+        return Response.success(PagedResponse.from(homeworks));
     }
 }
--- src/main/java/com/abcdedu_backend/homework/repository/HomeworkRepository.java
+++ src/main/java/com/abcdedu_backend/homework/repository/HomeworkRepository.java
@@ -1,7 +1,11 @@
 package com.abcdedu_backend.homework.repository;
 
 import com.abcdedu_backend.homework.entity.Homework;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HomeworkRepository extends JpaRepository<Homework, Long> {
+
+    Page<Homework> findAll(Pageable pageable);
 }
